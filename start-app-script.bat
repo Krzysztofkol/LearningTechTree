585b0d2e58184914eb936dd837bdacecce8e6c4f
Krzysztofkol <krzysztof.kolesnikowski@gmail.com>
2024-07-20 15:49:26 +0200

    changed project structure - main fronted and backend files

--- start-app-script.bat
+++ start-app-script.bat
@@ -2,14 +2,10 @@
 echo Starting Deep Learning Path Application...
 
 :: Start the backend (Flask server)
-start cmd /k "echo Starting Flask server... && python app.py"
+start cmd /k "echo Starting Flask server... && python backend.py"
 
 :: Wait for a moment to ensure the server has started
-timeout /t 1
+timeout /t .5
 
 :: Open the frontend in the default web browser
 start http://localhost:9696
-
-echo Application started. Check the opened command prompt for Flask server logs.
-echo Press any key to exit this window...
-pause >nul
